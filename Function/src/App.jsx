0173a046616704afba6d12834d73d60a78164bef
Banti Bohare <boharebanti@gmail.com>
2026-06-10 10:59:39 +0530

    Learn function and tailwind

---
+++ Function/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  function mouse(){
+    console.log("yyyyyyyeaaaaaaaaaaahhhhhhhhhhh")
+  }
+
+  return (
+    <>
+    <button onMouseOver={mouse}>Click Me</button>
+    </>
+  )
+}
+
+export default App
